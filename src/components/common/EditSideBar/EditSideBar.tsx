bfe67d30baaf4f12421d4d0093300995610bef61
MoonSoD <moonsod8@gmail.com>
2022-04-04 10:10:52 +0200

    Minor bugfix

--- src/components/common/EditSideBar/EditSideBar.tsx
+++ src/components/common/EditSideBar/EditSideBar.tsx
@@ -143,8 +143,10 @@ export const EditSideBar: FC<Props> = ({
           const isValid = await form.trigger();
           console.log(form.formState.errors);
           if (isValid) {
-            if (!form.getValues()?.id) {
-              form.setValue("id", editedObject?.id);
+            if (mode === "edit") {
+              if (!form.getValues()?.id) {
+                form.setValue("id", editedObject?.id);
+              }
             }
 
             onSubmit(form.getValues(), mode);
